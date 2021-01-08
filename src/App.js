c714a38ad8f0e2e9d36b5dd3ddb6968730b43ee6
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-01-08 14:28:42 +0200

    reference

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+    input1 = React.createRef();
+
+    render() {
+        return (
+            <div>
+                <input ref={this.input1} onInput={this.readInputValue}/>
+            </div>
+        );
+
+    }
+    readInputValue = () => {
+        console.log(this.input1.current.value);
+    };
+
 }
 
 export default App;
---
+++ src/components/Board.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+}
+
+
+
+// nothing!
